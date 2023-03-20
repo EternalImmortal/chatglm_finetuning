e5fa6f327e7dc554e7bbd0d2dfaa93494ad8e710
杏杏杏仁酱 <weijosephrj@gmail.com>
2023-03-20 18:16:47 +0800

    update

--- infer.py
+++ infer.py
@@ -3,18 +3,18 @@
 import os
 import torch
 from deep_training.data_helper import ModelArguments, TrainingArguments, DataArguments
-from deep_training.nlp.models.chatglm import TransformerChatGlmLMHeadModel, setup_model_profile, ChatGLMConfig,ChatGLMForConditionalGeneration
+from deep_training.nlp.models.chatglm import TransformerChatGlmLMHeadModel, setup_model_profile, ChatGLMConfig, \
+    ChatGLMForConditionalGeneration
 from deep_training.nlp.models.lora import LoraArguments
 from transformers import HfArgumentParser
 
-from data_utils import train_info_args, NN_DataHelper,get_deepspeed_config
+from data_utils import train_info_args, NN_DataHelper, get_deepspeed_config
 from tokenization_chatglm import ChatGLMTokenizer
 
 
 class MyTransformer(TransformerChatGlmLMHeadModel, with_pl=True):
     def __init__(self, *args, **kwargs):
         super(MyTransformer, self).__init__(*args, **kwargs)
-
 
 
 if __name__ == '__main__':
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
     dataHelper = NN_DataHelper(model_args, training_args, data_args)
     tokenizer: ChatGLMTokenizer
-    tokenizer, config, _,_ = dataHelper.load_tokenizer_and_config(
+    tokenizer, config, _, _ = dataHelper.load_tokenizer_and_config(
         tokenizer_class_name=ChatGLMTokenizer, config_class_name=ChatGLMConfig)
 
     # 官方28层
@@ -39,12 +39,17 @@ if __name__ == '__main__':
     base_model.half().to(torch.device('cuda:3'))
 
     with torch.inference_mode():
-        response, history = base_model.chat(tokenizer, "你好", history=[],max_length=1024)
-        print('你好',' ',response)
-
-        response, history = base_model.chat(tokenizer, "晚上睡不着应该怎么办", history=history,max_length=1024)
-        print('晚上睡不着应该怎么办',' ',response)
+        prefixs = [
+            "我想听一首开心的歌曲",
+            "周五下班了但工作没做完，不太开心",
+            "我想听听一首风格的西方通俗歌曲，希望它是国语，我希望它是原唱。"
+            "我想听听一首风格的西方通俗歌曲，希望它是中国话，它应该是一首演绎水平的歌曲，我希望听到爱情的感觉，我希望它是抖音。"
+            "上山打老虎的人应该听什么歌？"
+            "谈恋爱了，我应该听什么歌？"
+        ]
+        for question in prefixs:
+            response, history = base_model.chat(tokenizer, question, history=[], max_length=1024)
+            print(question, ' ', response)
 
         # response, history = base_model.chat(tokenizer, "写一个诗歌，关于冬天", history=[],max_length=30)
         # print('写一个诗歌，关于冬天',' ',response)
-
