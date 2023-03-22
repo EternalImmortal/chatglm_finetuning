a2a6245b8ea5f15828ad8951de8e346c33174c25
杏杏杏仁酱 <weijosephrj@gmail.com>
2023-03-22 17:28:20 +0800

    init

--- train.py
+++ train.py
@@ -199,7 +199,7 @@ if __name__ == '__main__':
         strategy = DeepSpeedStrategy(config=deepspeed_config, )
 
     trainer = Trainer(
-        callbacks=[checkpoint_callback],
+        # callbacks=[checkpoint_callback],
         max_epochs=training_args.max_epochs,
         max_steps=training_args.max_steps,
         accelerator="gpu", replace_sampler_ddp=False,
